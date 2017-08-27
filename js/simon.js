8c30885010faf4b9b2f43c09630ec97a47d4bf5c
Javier Nuñez <javier.nunez@solucionex.com>
2017-08-27 23:35:26 +0200

    key control

--- js/simon.js
+++ js/simon.js
@@ -1,6 +1,38 @@
 const levels = 15
 let keys = generateKeys(levels)
 
+function nextLevel(currentLevel) {
+  if (currentLevel == levels) {
+    alert ("Winer!!")
+  }
+  alert(`Level: ${currentLevel + 1}`)
+  for (let i = 0; i <= currentLevel; i++) {
+    setTimeout(() => activate(keys[i]),
+      1000 * (i+1))
+  }
+
+  let i = 0
+  let currentKey = keys[i]
+  window.addEventListener('keydown', onkeydown)
+  function onkeydown(ev) {
+    if (ev.keyCode == currentKey) {
+      activate(currentKey, { success: true })
+      i++
+      if (i > currentLevel) {
+        window.removeEventListener('keydown', onkeydown)
+        setTimeout(() => nextLevel(i), 1500)
+      }
+      currentKey = keys[i]
+    } else {
+      activate(currentKey, { fail: true })
+      window.removeEventListener('keydown', onkeydown)
+      setTimeout(() => alert('You lost :('), 1000)
+    }
+  }
+}
+
+nextLevel(0)
+
 function generateKeys(levels) {
   return new Array(levels).fill(0).map(generateRandomKey)
 }
@@ -8,7 +40,7 @@ function generateKeys(levels) {
 function generateRandomKey() {
   const min = 65
   const max = 90
-  return Math.round(Math.random()) * (max - min) + min
+  return Math.round(Math.random() * (max - min) + min)
 }
 
 function getElementByKeyCode(keyCode) {
